a3c85ecb3475aa89290f665d248509894bcfeeb7
iliya68rus <iliya97pro@yandex.ru>
2022-04-25 10:41:32 +0300

    test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Hibernate;
 
 public class Main {
     public static void main(String[] args) {
